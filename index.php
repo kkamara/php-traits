b3dc78d8f991414339ec809350f2683262e90df7
kkamara <kamaracomputers@gmail.com>
2023-11-29 13:43:57 +0000

    Use trait Say_World

--- index.php
+++ index.php
@@ -2,7 +2,13 @@
 
 trait Say_World {
     public function say_hello() {
-        echo "Hello Trait";
+        echo "Hello Trait".PHP_EOL;
     }
 }
 
+class Base {
+    use Say_World;
+}
+
+$b = new Base();
+$b->say_hello();
